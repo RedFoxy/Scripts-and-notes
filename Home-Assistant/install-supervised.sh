126c0c02c3990abc547891e1ec2dc3928f504636
RedFoxy Darrest <redfoxy@redfoxy.it>
2022-08-13 18:12:53 +0200

    v0.6 Improve check dependences and messages

--- Home-Assistant/install-supervised.sh
+++ Home-Assistant/install-supervised.sh
@@ -1,91 +1,117 @@
-###############################################################################
-# (unofficial) Home Assistant installation script
-# v0.4 11 August 2022
-# by Massimo "RedFoxy Darrest" Ciccio'
-#
-# https://github.com/RedFoxy/Scripts-and-notes
-#
-###############################################################################
-
 #!/bin/bash
+################################################################################
+#                                                                              #
+#                Home Assistant Supervised installation script                 #
+#                                 (unofficial)                                 #
+#                               v0.6 13/08/2022                                #
+#                     by Massimo "RedFoxy Darrest" Ciccio'                     #
+#       WebSite: https://redfoxy.it - GitHub: https://github.com/RedFoxy       #
+#   YouTube: https://youtube.redfoxy.it - Twitter: https://twitter.redfoxy.it  #
+#                                                                              #
+################################################################################
 
-# IMPORTANT!
-# Use this script as is it at your risk!
-# Use only on Debian
-# This script will install Docker and all dependeces to install
-# Home Assistant Supervised.
+################################################################################
+#                               Download and run:                              #
+#                   curl -fsSL ha-supervised.redfoxy.it | sh                   #
+################################################################################
 
-# How to run:
-#
-# curl -fsSL ha-supervised.redfoxy.it | sh
-#
-# Please, remeber to set a static ip on your computer
+NEED_SUDO=`which sudo`
+if [ `id -u` = 0 ]; then
+  NEED_SUDO="";
+else
+  if [ -z $NEED_SUDO ]; then
+    echo "Unable to find sudo in PATH! Please install before to continue.";
+    echo "Run as root: apt-get install sudo";
+    exit 2;
+  fi
+fi
 
-echo "########################################################################"
-echo "Welcome to the (unofficial) Home Assistant installation script";
-echo "";
-echo "This script was write by Massimo \"RedFoxy Darrest\" Ciccio' and you can"
-echo "download it from https://github.com/RedFoxy/Scripts-and-notes";
-echo "";
-echo "This script can be run on different systems, as Raspberry Pi, a virtual";
-echo "machine, and computer with cpu x86/amd64 and other hardware.";
-echo "It need to be run on a computer with Debian or Raspbian or other Debian";
-echo "like operative system, this script it'll use apt to manage packages and";
-echo "it'll install Docker and about all dependences required by Home";
-echo "Assistant, this installation it's a bit invasive so if you are not sure,";
-echo "please don't continue!";
-echo "";
-echo "To cancel the script please push CTRL C on your keyboar now!";
-echo "########################################################################"
-echo "";
 
-sec=10
+cat <<WELCOME
+################################################################################
+#                                                                              #
+#                Home Assistant Supervised installation script                 #
+#                                 (unofficial)                                 #
+#                               v0.6 13/08/2022                                #
+#                     by Massimo "RedFoxy Darrest" Ciccio'                     #
+#       WebSite: https://redfoxy.it - GitHub: https://github.com/RedFoxy       #
+#   YouTube: https://youtube.redfoxy.it - Twitter: https://twitter.redfoxy.it  #
+#                                                                              #
+################################################################################
 
-while [ $sec -ge 0 ]; do
- echo -ne "The installation will starts in: $sec\033[0K\r"
- let "sec=sec-1"
- sleep 1
+Home Assistant official WebSite: https://www.home-assistant.io/ 
+
+This script can be run on different systems, like a Raspberry Pi, a virtual
+machine, a computer with cpu x86 32/64 bit and other type hardware.
+To run you need to have Debian, Raspbian or Debian derivate operative system
+already installed, this script it'll uses apt-get to manage packages and it'll
+installs Docker and about all dependences required by Home Assistant, this
+installation it's a bit invasive so if you are not sure, please don't continue!
+
+                                     CAUTION
+                         ---> Use at your own risk! <---
+
+################################################################################
+
+WELCOME
+
+sec=20
+
+echo "Press any key to start install script, CTRL+c to cancel"
+while [ true ] ; do
+  read -t 1 -n 1
+  if [ $? = 0 ] ; then
+    break ;
+  else
+    if [ $sec -eq 0 ]; then
+      exit 2;
+    fi
+    let "sec=sec-1"
+  fi
 done
 
-
 # Update repository
-sudo apt-get update
+$NEED_SUDO apt-get update
+
 # Install dependences
-sudo apt-get install -y ca-certificates curl gnupg jq wget lsb-release systemd apparmor network-manager udisks2 libglib2.0-bin dbus
+$NEED_SUDO apt-get install -y ca-certificates curl gnupg jq wget lsb-release systemd apparmor network-manager udisks2 libglib2.0-bin dbus
 
 ARCH=$(uname -m)
 OSA=$(curl --silent "https://api.github.com/repos/home-assistant/os-agent/releases/latest" | jq -r .tag_name)
 
-# Apply docker gpg key
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-# Create Docker repository based on CPU
-echo \
- "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update repository with the new one
-sudo apt-get update
-
 # Install Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+curl -fsSL get.docker.com | sh
 
 # Download and install latest os-agent
 if [[ `wget -S --spider https://github.com/home-assistant/os-agent/releases/download/${OSA}/os-agent_${OSA}_linux_${ARCH}.deb 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
-  wget https://github.com/home-assistant/os-agent/releases/download/${OSA}/os-agent_${OSA}_linux_${ARCH}.deb
-  sudo dpkg -i os-agent_${OSA}_linux_${ARCH}.deb
-  rm -rf /tmp/install-ha-supervised
+  wget https://github.com/home-assistant/os-agent/releases/download/${OSA}/os-agent_${OSA}_linux_${ARCH}.deb -O /tmp/os-agent.deb
+  $NEED_SUDO dpkg -i /tmp/os-agent.deb
+  rm -rf /tmp/os-agent.deb
 else
   echo "Unable to find .deb file in https://github.com/home-assistant/os-agent/releases/latest"
   exit 1
 fi
 
 # Start the installation, please select your CPU/Release version
-wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
-sudo dpkg -i homeassistant-supervised.deb
+wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb -O /tmp/homeassistant-supervised.deb
+$NEED_SUDO dpkg -i /tmp/homeassistant-supervised.deb
+rm -rf /tmp/homeassistant-supervised.deb
 
-# Now save your data and reboot!
-# After reboot, Home Assistant need some minutes, it depends from your hardware, to complete the deploy,
-# you can check if it finish to install Home Assistant browsing http://IP-ADDRESS:8123 and checking if at the last
-# more than 3 containers are deployed, you can check the running container using: sudo docker ps
+cat <<ENDSCRIPT
+
+################################################################################
+
+Now save your data and reboot!
+
+After reboot, Home Assistant need some minutes, it depends from your hardware,
+to complete the containers deploy, you can check if it finish to install Home
+Assistant checking if at the last more than 3 containers are deployed, using the
+command sudo docker ps
+You'll can access to your Home Assistant browsing:
+
+$(ip a | grep "inet " | grep -vE "(127.0.0.1|docker|hassio)" | awk '!/([0-9]*\.){3}[0-9]*/  {print "http://"$2":8123/"}')
+
+Please, remeber to set a static ip address!
+
+################################################################################
+ENDSCRIPT
